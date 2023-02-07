f0d0cc432c2f902c640c3bcaa7fe0db97d71a500
ilfa <ilfahassan@gmail.com>
2023-02-07 17:33:56 +0000

    deleted react logo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders star war heading', () => {
   render(<App />);
   const linkElement = screen.getByText(/star war/i);
   expect(linkElement).toBeInTheDocument();
